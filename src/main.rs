fb7022b42d84c6c330e0c8c85587b67e26774d4f
Insomnia <me@me.com>
2025-11-06 17:19:20 +0100

    Refine `--mode` handling for CLI; integrate validation and limit processing to Wireshark mode
    Signed-off-by: Insomnia <me@me.com>

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,39 @@ use modules::wireshark::analyse_path::{Protocol, UniqueIp, set_debug_chain, anal
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let mut debug_chain = false;
+    let mut mode: String = "None".to_string();
+
     if args.len() < 2 {
         println!("Argument file is missing. <file>");
         exit(0);
     }
 
+    if !args.iter().any(|arg| arg == "--mode") {
+        println!("Argument --mode is missing.");
+        exit(1)
+    } else {
+        if let Some(pos) = args.iter().position(|arg| arg == "--mode") {
+
+            let mode_pos = &args[pos + 1].as_str();
+
+            if !["wireshark"].contains(mode_pos) {
+                println!("Invalid mode. only (wireshark) is supported.");
+                exit(1)
+            } else {
+                mode = mode_pos.to_string();
+            }
+
+        }
+    }
+
     let file = Some(&args[1]).unwrap();
-    let mut debug_chain = false;
 
     for arg in args.iter().skip(1) {
         match arg.as_str() {
             "--debug-chain" => debug_chain = true,
             "--help" => {
-                println!("\nUsage: monica <file>");
+                println!("\nUsage: monica <file> <--mode>");
                 println!("\nOptions");
                 println!("  --debug-chain   Enable debug mode but more slow.");
                 println!("  --mode          wireshark");
@@ -39,50 +59,52 @@ fn main() {
 
     set_debug_chain(debug_chain);
 
-    let mut counts_pro: HashMap<Protocol, usize> = HashMap::new();
-    let mut count_number_of_packets: usize = 0;
+    if mode == "wireshark" {
+        let mut counts_pro: HashMap<Protocol, usize> = HashMap::new();
+        let mut count_number_of_packets: usize = 0;
 
-    let mut counts_ip: HashMap<UniqueIp, usize> = HashMap::new();
+        let mut counts_ip: HashMap<UniqueIp, usize> = HashMap::new();
 
-    // Creates a builder with needed tshark parameters
-    let builder = rtshark::RTSharkBuilder::builder()
-        .input_path(file);
+        // Creates a builder with needed tshark parameters
+        let builder = rtshark::RTSharkBuilder::builder()
+            .input_path(file);
 
-    // Start a new TShark process
-    let mut rtshark = match builder.spawn() {
-        Err(err) =>  { eprintln!("Error running tshark: {err}"); return }
-        Ok(rtshark) => rtshark,
-    };
+        // Start a new TShark process
+        let mut rtshark = match builder.spawn() {
+            Err(err) =>  { eprintln!("Error running tshark: {err}"); return }
+            Ok(rtshark) => rtshark,
+        };
 
-    // Read packets until the end of the PCAP file
-    while let Some(packet) = rtshark.read().unwrap() {
-        for layer in packet {
+        // Read packets until the end of the PCAP file
+        while let Some(packet) = rtshark.read().unwrap() {
+            for layer in packet {
 
-            let proto = Protocol {name: layer.name().to_string()};
-            *counts_pro.entry(proto).or_insert(0) += 1;
-            count_number_of_packets += 1;
+                let proto = Protocol {name: layer.name().to_string()};
+                *counts_pro.entry(proto).or_insert(0) += 1;
+                count_number_of_packets += 1;
 
-            for metadata in layer {
-                if metadata.name() == "ip.src" {
+                for metadata in layer {
+                    if metadata.name() == "ip.src" {
 
-                    let ip = UniqueIp {src: metadata.value().to_string()};
-                    *counts_ip.entry(ip).or_insert(0) += 1;
+                        let ip = UniqueIp {src: metadata.value().to_string()};
+                        *counts_ip.entry(ip).or_insert(0) += 1;
+                    }
                 }
             }
+        };
+
+        for (protocol, size) in &counts_pro {
+            println!("{:?} and count : {:?}", protocol.name, size);
         }
-    };
 
-    for (protocol, size) in &counts_pro {
-        println!("{:?} and count : {:?}", protocol.name, size);
+        println!("\n\n");
+
+        for (ip, size) in &counts_ip {
+            println!("{:?} and count : {:?}", ip.src, size);
+        }
+
+        println!("\n\nNumber of packets: {}", count_number_of_packets);
+
+        analyse_path(counts_ip, counts_pro, file);
     }
-
-    println!("\n\n");
-
-    for (ip, size) in &counts_ip {
-        println!("{:?} and count : {:?}", ip.src, size);
-    }
-
-    println!("\n\nNumber of packets: {}", count_number_of_packets);
-
-    analyse_path(counts_ip, counts_pro, file);
 }
